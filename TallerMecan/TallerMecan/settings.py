f3f10d01a86423c8615aceaa6e84c6bd92dbf56c
maximilianosanla <maximiliano.sanchez.lara@gmail.com>
2025-05-25 22:56:41 -0800

    update allowed hosts

--- TallerMecan/TallerMecan/settings.py
+++ TallerMecan/TallerMecan/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['52.205.245.143', '127.0.0.1', 'localhost']
